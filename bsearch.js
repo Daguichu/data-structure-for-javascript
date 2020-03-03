234bc1670f252ffbc7aa37266cd6c0eecbd06c18
chuchen <18725716749@163.com>
2020-03-03 09:42:57 +0800

    add search func

--- bsearch.js
+++ bsearch.js
@@ -3,10 +3,10 @@ function bsearch(arr, num) {
   let low = 0,
     high = len - 1;
   while (low <= high) {
-    let mid = (low + high) / 2;
-    if (a[mid] == num) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] == num) {
       return mid;
-    } else if (a[mid] < num) {
+    } else if (arr[mid] < num) {
       low = mid + 1;
     } else {
       high = mid - 1;
@@ -21,7 +21,7 @@ function bsearch2(arr, num) {
 }
 
 function search(arr, low, high, num) {
-  let mid = (low + high) / 2;
+  let mid = Math.floor((low + high) / 2);
   if (low > high) return -1;
   if (arr[mid] === num) return mid;
   if (arr[mid] < num) {
@@ -30,3 +30,89 @@ function search(arr, low, high, num) {
     return search(arr, low, mid - 1, num);
   }
 }
+
+//查找第一个等于num的位置
+function searchFirst(arr, num) {
+  const len = arr.length;
+  let low = 0,
+    high = len - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if (num > arr[mid]) {
+      low = mid + 1;
+    } else if (num < arr[mid]) {
+      high = mid - 1;
+    } else {
+      if (mid === 0 || arr[mid - 1] !== num) {
+        return mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+
+//查找最后一个等于num的位置
+function searchEnd(arr, num) {
+  const len = arr.length;
+  let low = 0,
+    high = len - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if (num > arr[mid]) {
+      low = mid + 1;
+    } else if (num < arr[mid]) {
+      high = mid - 1;
+    } else {
+      if (mid === len - 1 || arr[mid + 1] !== num) {
+        return mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+  }
+  return -1;
+}
+
+//查找第一个大于等于num的位置
+function searchFirstBig(arr, num) {
+  const len = arr.length;
+  let low = 0,
+    high = len - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if (arr[mid] >= num) {
+      if (mid == 0 || arr[mid - 1] < num) return mid;
+      else high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+
+//查找最后一个小于等于num的位置
+function searchLastSmall(arr, num) {
+  const len = arr.length;
+  let low = 0,
+    high = len - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + ((high - low) >> 1));
+    if (arr[mid] > num) {
+      high = mid - 1;
+    } else {
+      if (mid == len - 1 || arr[mid + 1] > num) return mid;
+      else low = mid + 1;
+    }
+  }
+}
+const arr = [3, 5, 6, 6, 6, 8, 9, 10];
+console.log(bsearch(arr, 6));
+console.log(bsearch2(arr, 6));
+console.log(searchFirst(arr, 6));
+console.log(searchEnd(arr, 6));
+console.log(searchFirstBig(arr, 7));
+console.log(searchFirstBig(arr, 6));
+console.log(searchLastSmall(arr, 8));
+console.log(searchLastSmall(arr, 6));
